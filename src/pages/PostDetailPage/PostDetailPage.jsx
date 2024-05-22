6bec927b2434238e7f7147ab6d90a5145acf25cd
hb9901 <hb9901@gmail.com>
2024-05-22 09:55:59 +0900

    fix: postDetailPage 컴포넌트 분리

--- src/pages/PostDetailPage/PostDetailPage.jsx
+++ src/pages/PostDetailPage/PostDetailPage.jsx
@@ -1,76 +1,18 @@
-import { useParams } from "react-router-dom";
 import styled from "styled-components";
-import useInput from "../../hooks/useInput";
+import Expenditure from "../../components/Expenditure";
 
 const PageWrapper = styled.div``;
 
-const InputWrapper = styled.div``;
-
-const Label = styled.div``;
-
-const Input = styled.input``;
-
-const ButtonWrapper = styled.div``;
-
-const ModifyButton = styled.button``;
-
-const DeleletButton = styled.button``;
-
-const GoBackButton = styled.button``;
 
 function PostDetailPage({ monthExpenditures , updateExpenditure, deleteExpenditure}) {
-  const params = useParams();
-  const expenditure = monthExpenditures.find(
-    (expenditure) => expenditure.id === params.id
-  );
-  const [modifiedExpenditure, handleModifiedExpenditure] = useInput(expenditure);
-
-  const handleClickModify = () =>{
-    updateExpenditure(params.id, modifiedExpenditure);
-  }
+  
   return (
     <PageWrapper>
-      <InputWrapper>
-        <Label>날짜</Label>
-        <Input
-          type="text"
-          value={modifiedExpenditure.date}
-          data-type="date"
-          onChange={handleModifiedExpenditure}
-        />
-      </InputWrapper>
-      <InputWrapper>
-        <Label>항목</Label>
-        <Input
-          type="text"
-          value={modifiedExpenditure.item}
-          data-type="date"
-          onChange={handleModifiedExpenditure}
-        />
-      </InputWrapper>
-      <InputWrapper>
-        <Label>금액</Label>
-        <Input
-          type="text"
-          value={modifiedExpenditure.amount}
-          data-type="amount"
-          onChange={handleModifiedExpenditure}
-        />
-      </InputWrapper>
-      <InputWrapper>
-        <Label>내용</Label>
-        <Input
-          type="text"
-          value={modifiedExpenditure.description}
-          data-type="description"
-          onChange={handleModifiedExpenditure}
-        />
-      </InputWrapper>
-      <ButtonWrapper>
-        <ModifyButton onClick={handleClickModify}>수정</ModifyButton>
-        <DeleletButton>삭제</DeleletButton>
-        <GoBackButton>뒤로 가기</GoBackButton>
-      </ButtonWrapper>
+      <Expenditure
+        monthExpenditures={monthExpenditures}
+        updateExpenditure={updateExpenditure}
+        deleteExpenditure={deleteExpenditure}
+      />
     </PageWrapper>
   );
 }
