bd2d46c3221cca9ed4300ffc031b3cddcdf7f0a7
hb9901 <hb9901@gmail.com>
2024-05-22 12:01:21 +0900

    style: global 배경색 설정

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -4,8 +4,11 @@ const GlobalStyle = createGlobalStyle`
   body {
     width: 100%;
     max-width: 1280px;
+
     margin: 0 auto;
     padding: 2rem;
+    
+    background-color: #2ec4b6;;
   }
 `;
 
