f65a61ec54a14cb9da28b13a5dc44e88c1a4b961
hb9901 <hb9901@gmail.com>
2024-05-22 09:49:43 +0900

    fix: homepageWrapper 생성

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,17 @@
+import styled from "styled-components";
 import Expenditures from "../../components/Expenditures";
 import InputForm from "../../components/InputForm";
 import Months from "../../components/Months";
 
+const PageWrapper = styled.div``;
+
 function HomePage({ addExpenditure, setMonth, monthExpenditures }) {
   return (
-    <div>
+    <PageWrapper>
       <InputForm addExpenditure={addExpenditure} />
       <Months setMonth={setMonth} />
       <Expenditures monthExpenditures={monthExpenditures} />
-    </div>
+    </PageWrapper>
   );
 }
 
