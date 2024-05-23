e2ecde1ccff4f14c740c4db75caf9da4aa26be5d
hb9901 <hb9901@gmail.com>
2024-05-23 12:39:58 +0900

    update: graph 아이템 리스트 구현 && graph 로직 수정

--- src/components/ExpenditureGraph/ExpenditureGraph.jsx
+++ src/components/ExpenditureGraph/ExpenditureGraph.jsx
@@ -1,37 +1,46 @@
 import styled from "styled-components";
+import {
+  calPercentage,
+  calTotalCost,
+  makeCostStr,
+  makeGraphItems,
+  makeItemsExpenditureList,
+  makeItemsExpenditureObj,
+} from "./fucntions";
 
 function ExpenditureGraph({ monthExpenditures, curMonth }) {
-  let total = 0;
-  let itemsExpenditureList = [];
-  let graphItems = [];
-  const itemsExpenditureObj = {};
-
-  monthExpenditures.forEach((expenditure) => {
-    const { item, amount } = expenditure;
-
-    if (itemsExpenditureObj[item]) itemsExpenditureObj[item] += amount;
-    else itemsExpenditureObj[item] = amount;
-    total += amount;
-  });
-
-  itemsExpenditureList = Object.entries(itemsExpenditureObj).sort(
-    (a, b) => b[1] - a[1]
-  );
-  graphItems = itemsExpenditureList.map(
-    (itemExpenditure) => (itemExpenditure[1] /= total / 100)
-  );
-
-  console.log(graphItems);
+  const TOTAL_COST = calTotalCost(monthExpenditures);
+  const totalStr = makeCostStr(TOTAL_COST);
+  const itemsExpenditureObj = makeItemsExpenditureObj(monthExpenditures);
+  const itemsExpenditureList = makeItemsExpenditureList(itemsExpenditureObj);
+  const graphItems = makeGraphItems(itemsExpenditureList);
 
   return (
     <>
-      <Title>{`${curMonth + 1}월 총 지출: ${total} 원`}</Title>
+      <Title>{`${curMonth + 1}월 총 지출: ${totalStr} 원`}</Title>
       <Graph>
         {graphItems.map((graphItem, index) => {
-          return <GraphItem key={index} width={graphItem} index={index} />;
+          return (
+            <GraphItem
+              key={index}
+              width={calPercentage(graphItem[1], TOTAL_COST)}
+              index={index}
+            />
+          );
         })}
       </Graph>
-      <Items></Items>
+      <Items>
+        {graphItems.map((graphItem, index) => {
+          return (
+            <Item key={index}>
+              <ItemColor index={index} />
+              {`${graphItem[0]}: ${makeCostStr(
+                graphItem[1]
+              )} 원 (${calPercentage(graphItem[1], TOTAL_COST)}%)`}
+            </Item>
+          );
+        })}
+      </Items>
     </>
   );
 }
@@ -39,21 +48,23 @@ function ExpenditureGraph({ monthExpenditures, curMonth }) {
 export default ExpenditureGraph;
 
 const Title = styled.div`
-  padding: 20px;
-  background-color: rgb(255, 255, 255);
-  border-radius: 16px;
-  text-align: center;
   font-size: 18px;
   font-weight: bold;
+
+  background-color: rgb(255, 255, 255);
 `;
 const Graph = styled.div`
   display: flex;
   align-items: center;
+
   margin-top: 20px;
+
   height: 40px;
-  background-color: rgb(233, 236, 239);
   border-radius: 8px;
+
   overflow: hidden;
+
+  background-color: rgb(233, 236, 239);
 `;
 
 const GraphItem = styled.div`
@@ -75,4 +86,45 @@ const GraphItem = styled.div`
   }}
 `;
 
-const Items = styled.div``;
+const Items = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+
+  gap: 20px;
+  margin-top: 10px;
+`;
+
+const Item = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  font-size: 14px;
+  font-weight: bold;
+  color: rgb(85, 85, 85);
+`;
+
+const ItemColor = styled.div`
+  width: 20px;
+  height: 10px;
+
+  margin-right: 8px;
+
+  ${({ index }) => {
+    switch (index) {
+      case 0:
+        return `background-color: rgb(0, 123, 255);`;
+      case 1:
+        return `background-color: rgb(40, 167, 69);`;
+      case 2:
+        return `background-color: rgb(220, 53, 69);`;
+      case 3:
+        return `background-color: rgb(255, 193, 7);`;
+      default:
+        return `background-color: rgb(23, 162, 184);`;
+    }
+  }}
+`;
