51bd2c9604e0bc49ab34c04cd854984ec09dfb69
hb9901 <hb9901@gmail.com>
2024-05-27 19:42:24 +0900

    fix: style-components에 전달하는 props 이름 수정

--- src/components/ExpenditureGraph/ExpenditureGraph.jsx
+++ src/components/ExpenditureGraph/ExpenditureGraph.jsx
@@ -23,8 +23,8 @@ function ExpenditureGraph({ monthExpenditures, curMonth }) {
           return (
             <GraphItem
               key={index}
-              width={calPercentage(graphItem[1], TOTAL_COST)}
-              index={index}
+              $width={calPercentage(graphItem[1], TOTAL_COST)}
+              $index={index}
             />
           );
         })}
@@ -33,7 +33,7 @@ function ExpenditureGraph({ monthExpenditures, curMonth }) {
         {graphItems.map((graphItem, index) => {
           return (
             <Item key={index}>
-              <ItemColor index={index} />
+              <ItemColor $index={index} />
               {`${graphItem[0]}: ${makeCostStr(
                 graphItem[1]
               )} 원 (${calPercentage(graphItem[1], TOTAL_COST)}%)`}
@@ -69,9 +69,9 @@ const Graph = styled.div`
 
 const GraphItem = styled.div`
   height: 100%;
-  ${({ width }) => `width: ${width}%;`}
-  ${({ index }) => {
-    switch (index) {
+  ${({ $width }) => `width: ${$width}%;`}
+  ${({ $index }) => {
+    switch ($index) {
       case 0:
         return `background-color: rgb(0, 123, 255);`;
       case 1:
@@ -113,8 +113,8 @@ const ItemColor = styled.div`
 
   margin-right: 8px;
 
-  ${({ index }) => {
-    switch (index) {
+  ${({ $index }) => {
+    switch ($index) {
       case 0:
         return `background-color: rgb(0, 123, 255);`;
       case 1:
